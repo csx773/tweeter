c4005e85f6c134a3a7e916be5ca56460ce143708
Chris Xiao <csx773@gmail.com>
2019-04-16 23:23:04 +0000

    Finished char counter function with colors updates

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,24 @@
 
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log('Document is ready ==> LOADED UP CHAR COUNTER .JS');
-  const textbox = document.querySelector('#current-tweet');
-
+  console.log('Document is ready!! Inside composer-char-counter.js');
 
   $("#current-tweet").on('input', function(event) {
-    console.log('INSIDE change event');
+    //console.log('INSIDE change event');
     let length = $(this).val().length;
     let characterLeft = 140 - length;
     // find the innerText of <span> by going up one node and searching down
     let counterElm = $(this).parent().find("span")
 
     if(characterLeft >= 0){
-      //updated counter in HTML
+      //updated counter in HTML, sets text color to black
+      counterElm.css("color", "black");
       counterElm.text(characterLeft);
     } else {
       // turn the counter text to red and update
-
+      counterElm.css("color", "red");
+      counterElm.text(characterLeft);
     }
-
 
   });
 
